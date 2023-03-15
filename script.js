4edf41070b969f0d7f3b94f24b061fa08e8ab2c0
gilberto mendoza <gilbertomf2801@gmail.com>
2023-03-14 21:13:04 -0300

    Pre entrega2

---
+++ script.js
@@ -0,0 +1,56 @@
+const temp = document.getElementById("temp"),
+  date = document.getElementById("date-time"),
+  condition = document.getElementById("condition"),
+  rain = document.getElementById("rain"),
+  mainIcon = document.getElementById("icon"),
+  currentLocation = document.getElementById("location"),
+  uvIndex = document.querySelector(".uv-index"),
+  uvText = document.querySelector(".uv-text"),
+  windSpeed = document.querySelector(".wind-speed"),
+  sunRise = document.querySelector(".sun-rise"),
+  sunSet = document.querySelector(".sun-set"),
+  humidity = document.querySelector(".humidity"),
+  visibilty = document.querySelector(".visibilty"),
+  humidityStatus = document.querySelector(".humidity-status"),
+  airQuality = document.querySelector(".air-quality"),
+  airQualityStatus = document.querySelector(".air-quality-status"),
+  visibilityStatus = document.querySelector(".visibilty-status"),
+  searchForm = document.querySelector("#search"),
+  search = document.querySelector("#query"),
+  celciusBtn = document.querySelector(".celcius"),
+  fahrenheitBtn = document.querySelector(".fahrenheit"),
+  tempUnit = document.querySelectorAll(".temp-unit"),
+  hourlyBtn = document.querySelector(".hourly"),
+  weekBtn = document.querySelector(".week"),
+  weatherCards = document.querySelector("#weather-cards");
+
+let currentCity = "";
+let currentUnit = "c";
+let hourlyorWeek = "week";
+
+// function to get date and time
+function getDateTime() {
+  let now = new Date(),
+    hour = now.getHours(),
+    minute = now.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  // 12 hours format
+  hour = hour % 12;
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  let dayString = days[now.getDay()];
+  return `${dayString}, ${hour}:${minute}`;
+}

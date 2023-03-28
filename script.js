9da0b76036d7ea08897457c4de8f8e3ca8b947ae
gilberto mendoza <gilbertomf2801@gmail.com>
2023-03-28 19:27:23 -0300

    Pre entrega 3

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let currentCity = "";
 let currentUnit = "c";
 let hourlyorWeek = "week";
 
-// function to get date and time
+// obtener fecha y hora
 function getDateTime() {
   let now = new Date(),
     hour = now.getHours(),
@@ -43,7 +43,7 @@ function getDateTime() {
     "Friday",
     "Saturday",
   ];
-  // 12 hours format
+  // formato 12 horas
   hour = hour % 12;
   if (hour < 10) {
     hour = "0" + hour;
@@ -54,3 +54,369 @@ function getDateTime() {
   let dayString = days[now.getDay()];
   return `${dayString}, ${hour}:${minute}`;
 }
+
+//actualizando fecha y hora
+date.innerText = getDateTime();
+setInterval(() => {
+  date.innerText = getDateTime();
+}, 1000);
+
+// IP publico
+function getPublicIp() {
+  fetch("https://geolocation-db.com/json/", {
+    method: "GET",
+    headers: {},
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      currentCity = data.city;
+      getWeatherData(data.city, currentUnit, hourlyorWeek);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+getPublicIp();
+
+// API
+function getWeatherData(city, unit, hourlyorWeek) {
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=EJ6UBL2JEQGYB3AA4ENASN62J&contentType=json`,
+    {
+      method: "GET",
+      headers: {},
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let today = data.currentConditions;
+      if (unit === "c") {
+        temp.innerText = today.temp;
+      } else {
+        temp.innerText = celciusToFahrenheit(today.temp);
+      }
+      currentLocation.innerText = data.resolvedAddress;
+      condition.innerText = today.conditions;
+      rain.innerText = "Perc - " + today.precip + "%";
+      uvIndex.innerText = today.uvindex;
+      windSpeed.innerText = today.windspeed;
+      measureUvIndex(today.uvindex);
+      mainIcon.src = getIcon(today.icon);
+      changeBackground(today.icon);
+      humidity.innerText = today.humidity + "%";
+      updateHumidityStatus(today.humidity);
+      visibilty.innerText = today.visibility;
+      updateVisibiltyStatus(today.visibility);
+      airQuality.innerText = today.winddir;
+      updateAirQualityStatus(today.winddir);
+      if (hourlyorWeek === "hourly") {
+        updateForecast(data.days[0].hours, unit, "day");
+      } else {
+        updateForecast(data.days, unit, "week");
+      }
+      sunRise.innerText = covertTimeTo12HourFormat(today.sunrise);
+      sunSet.innerText = covertTimeTo12HourFormat(today.sunset);
+    })
+    .catch((err) => {
+      alert("City not found in our database");
+    });
+}
+
+//actualizar clima
+function updateForecast(data, unit, type) {
+  weatherCards.innerHTML = "";
+  let day = 0;
+  let numCards = 0;
+  if (type === "day") {
+    numCards = 24;
+  } else {
+    numCards = 7;
+  }
+  for (let i = 0; i < numCards; i++) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    let dayName = getHour(data[day].datetime);
+    if (type === "week") {
+      dayName = getDayName(data[day].datetime);
+    }
+    let dayTemp = data[day].temp;
+    if (unit === "f") {
+      dayTemp = celciusToFahrenheit(data[day].temp);
+    }
+    let iconCondition = data[day].icon;
+    let iconSrc = getIcon(iconCondition);
+    let tempUnit = "°C";
+    if (unit === "f") {
+      tempUnit = "°F";
+    }
+    card.innerHTML = `
+                <h2 class="day-name">${dayName}</h2>
+            <div class="card-icon">
+              <img src="${iconSrc}" class="day-icon" alt="" />
+            </div>
+            <div class="day-temp">
+              <h2 class="temp">${dayTemp}</h2>
+              <span class="temp-unit">${tempUnit}</span>
+            </div>
+  `;
+    weatherCards.appendChild(card);
+    day++;
+  }
+}
+
+//iconos clima
+function getIcon(condition) {
+  if (condition === "partly-cloudy-day") {
+    return "https://i.ibb.co/PZQXH8V/27.png";
+  } else if (condition === "partly-cloudy-night") {
+    return "https://i.ibb.co/Kzkk59k/15.png";
+  } else if (condition === "rain") {
+    return "https://i.ibb.co/kBd2NTS/39.png";
+  } else if (condition === "clear-day") {
+    return "https://i.ibb.co/rb4rrJL/26.png";
+  } else if (condition === "clear-night") {
+    return "https://i.ibb.co/1nxNGHL/10.png";
+  } else {
+    return "https://i.ibb.co/rb4rrJL/26.png";
+  }
+}
+
+// fondo dependiendo del clima
+function changeBackground(condition) {
+  const body = document.querySelector("body");
+  let bg = "";
+  if (condition === "partly-cloudy-day") {
+    bg = "https://i.ibb.co/qNv7NxZ/pc.webp";
+  } else if (condition === "partly-cloudy-night") {
+    bg = "https://i.ibb.co/RDfPqXz/pcn.jpg";
+  } else if (condition === "rain") {
+    bg = "https://i.ibb.co/h2p6Yhd/rain.webp";
+  } else if (condition === "clear-day") {
+    bg = "https://i.ibb.co/WGry01m/cd.jpg";
+  } else if (condition === "clear-night") {
+    bg = "https://i.ibb.co/kqtZ1Gx/cn.jpg";
+  } else {
+    bg = "https://i.ibb.co/qNv7NxZ/pc.webp";
+  }
+  body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${bg})`;
+}
+
+
+function getHour(time) {
+  let hour = time.split(":")[0];
+  let min = time.split(":")[1];
+  if (hour > 12) {
+    hour = hour - 12;
+    return `${hour}:${min} PM`;
+  } else {
+    return `${hour}:${min} AM`;
+  }
+}
+
+// formato de hora, 12 horas
+function covertTimeTo12HourFormat(time) {
+  let hour = time.split(":")[0];
+  let minute = time.split(":")[1];
+  let ampm = hour >= 12 ? "pm" : "am";
+  hour = hour % 12;
+  hour = hour ? hour : 12;
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  let strTime = hour + ":" + minute + " " + ampm;
+  return strTime;
+}
+
+// funcion para obtener cualquier nombre 
+function getDayName(date) {
+  let day = new Date(date);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day.getDay()];
+}
+
+// UV index status
+function measureUvIndex(uvIndex) {
+  if (uvIndex <= 2) {
+    uvText.innerText = "Low";
+  } else if (uvIndex <= 5) {
+    uvText.innerText = "Moderate";
+  } else if (uvIndex <= 7) {
+    uvText.innerText = "High";
+  } else if (uvIndex <= 10) {
+    uvText.innerText = "Very High";
+  } else {
+    uvText.innerText = "Extreme";
+  }
+}
+
+// status humedad
+function updateHumidityStatus(humidity) {
+  if (humidity <= 30) {
+    humidityStatus.innerText = "Low";
+  } else if (humidity <= 60) {
+    humidityStatus.innerText = "Moderate";
+  } else {
+    humidityStatus.innerText = "High";
+  }
+}
+
+// visibilidad status
+function updateVisibiltyStatus(visibility) {
+  if (visibility <= 0.03) {
+    visibilityStatus.innerText = "Dense Fog";
+  } else if (visibility <= 0.16) {
+    visibilityStatus.innerText = "Moderate Fog";
+  } else if (visibility <= 0.35) {
+    visibilityStatus.innerText = "Light Fog";
+  } else if (visibility <= 1.13) {
+    visibilityStatus.innerText = "Very Light Fog";
+  } else if (visibility <= 2.16) {
+    visibilityStatus.innerText = "Light Mist";
+  } else if (visibility <= 5.4) {
+    visibilityStatus.innerText = "Very Light Mist";
+  } else if (visibility <= 10.8) {
+    visibilityStatus.innerText = "Clear Air";
+  } else {
+    visibilityStatus.innerText = "Very Clear Air";
+  }
+}
+
+// calidad de aire status
+function updateAirQualityStatus(airquality) {
+  if (airquality <= 50) {
+    airQualityStatus.innerText = "Good👌";
+  } else if (airquality <= 100) {
+    airQualityStatus.innerText = "Moderate😐";
+  } else if (airquality <= 150) {
+    airQualityStatus.innerText = "Unhealthy for Sensitive Groups😷";
+  } else if (airquality <= 200) {
+    airQualityStatus.innerText = "Unhealthy😷";
+  } else if (airquality <= 250) {
+    airQualityStatus.innerText = "Very Unhealthy😨";
+  } else {
+    airQualityStatus.innerText = "Hazardous😱";
+  }
+}
+
+// search form
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let location = search.value;
+  if (location) {
+    currentCity = location;
+    getWeatherData(location, currentUnit, hourlyorWeek);
+  }
+});
+
+// celcius a fahrenheit
+function celciusToFahrenheit(temp) {
+  return ((temp * 9) / 5 + 32).toFixed(1);
+}
+
+
+var currentFocus;
+search.addEventListener("input", function (e) {
+  removeSuggestions();
+  var a,
+    b,
+    i,
+    val = this.value;
+  if (!val) {
+    return false;
+  }
+  currentFocus = -1;
+
+  a = document.createElement("ul");
+  a.setAttribute("id", "suggestions");
+
+  this.parentNode.appendChild(a);
+
+  for (i = 0; i < cities.length; i++) {
+    if (
+      cities[i].name.substr(0, val.length).toUpperCase() == val.toUpperCase()
+    ) {
+      b = document.createElement("li");
+      b.innerHTML =
+        "<strong>" + cities[i].name.substr(0, val.length) + "</strong>";
+      b.innerHTML += cities[i].name.substr(val.length);
+      b.innerHTML += "<input type='hidden' value='" + cities[i].name + "'>";
+      b.addEventListener("click", function (e) {
+        search.value = this.getElementsByTagName("input")[0].value;
+        removeSuggestions();
+      });
+
+      a.appendChild(b);
+    }
+  }
+});
+
+search.addEventListener("keydown", function (e) {
+  var x = document.getElementById("suggestions");
+  if (x) x = x.getElementsByTagName("li");
+  if (e.keyCode == 40) {
+    currentFocus++;
+    addActive(x);
+  } else if (e.keyCode == 38) {
+    currentFocus--;
+    addActive(x);
+  }
+  if (e.keyCode == 13) {
+    e.preventDefault();
+    if (currentFocus > -1) {
+      if (x) x[currentFocus].click();
+    }
+  }
+});
+
+
+// cambio de unidades
+function changeUnit(unit) {
+  if (currentUnit !== unit) {
+    currentUnit = unit;
+    tempUnit.forEach((elem) => {
+      elem.innerText = `°${unit.toUpperCase()}`;
+    });
+    if (unit === "c") {
+      celciusBtn.classList.add("active");
+      fahrenheitBtn.classList.remove("active");
+    } else {
+      celciusBtn.classList.remove("active");
+      fahrenheitBtn.classList.add("active");
+    }
+    getWeatherData(currentCity, currentUnit, hourlyorWeek);
+  }
+}
+
+hourlyBtn.addEventListener("click", () => {
+  changeTimeSpan("hourly");
+});
+weekBtn.addEventListener("click", () => {
+  changeTimeSpan("week");
+});
+
+// funcion por hora y semana
+function changeTimeSpan(unit) {
+  if (hourlyorWeek !== unit) {
+    hourlyorWeek = unit;
+    if (unit === "hourly") {
+      hourlyBtn.classList.add("active");
+      weekBtn.classList.remove("active");
+    } else {
+      hourlyBtn.classList.remove("active");
+      weekBtn.classList.add("active");
+    }
+    getWeatherData(currentCity, currentUnit, hourlyorWeek);
+  }
+}
+
+
+
+
